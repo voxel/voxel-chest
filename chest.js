8bcc3aa6e39d7646bdba3be48ed6c1dd10ba3f06
deathcap <deathcap@gmx.co.uk>
2016-03-27 21:25:10 -0700

    Continue porting to ES6

--- chest.js
+++ chest.js
@@ -5,7 +5,7 @@ const Inventory = require('inventory');
 const InventoryWindow = require('inventory-window');
 const ItemPile = require('itempile');
 
-module.exports = (game, opts) => return new Chest(game, opts);
+module.exports = (game, opts) => new Chest(game, opts);
 
 module.exports.pluginInfo = {
   loadAfter: ['voxel-blockdata', 'voxel-registry', 'voxel-recipes', 'voxel-carry']
@@ -15,7 +15,7 @@ class Chest {
   constructor(game, opts) {
     this.game = game;
     if (!game.plugins.get('voxel-carry')) throw new Error('voxel-chest requires "voxel-carry" plugin');
-    playerInventory = game.plugins.get('voxel-carry').inventory;
+    this.playerInventory = game.plugins.get('voxel-carry').inventory;
 
     this.registry = game.plugins.get('voxel-registry') ? game.plugins.get('voxel-registry') : undefined;
     this.recipes = game.plugins.get('voxel-recipes') ? game.plugins.get('voxel-recipes') : undefined;
@@ -53,66 +53,75 @@ class Chest {
   }
 }
 
-// TODO: port the rest
 class ChestDialog extends InventoryDialog {
-  constructor(this.game, this.playerInventory, this.registry, this.blockdata) {
-
-    super game,
-      playerLinkedInventory: this.chestInventory
-      upper: [chestCont]
+  constructor(game, playerInventory, registry, blockdata) {
+    super(game,
+      {
+        playerLinkedInventory: this.chestInventory,
+        upper: [chestCont]
+      });
+    this.game = game;
+    this.playerInventory = playerInventory;
+    this.registry = registry;
+    this.blockdata = blockdata;
+  }
 
   static setup(opts) {
     this.chestInventory = new Inventory(10, 3)
-    this.chestInventory.on 'changed', () => this.updateBlockdata()
-    this.chestIW = new InventoryWindow {inventory:this.chestInventory, registry:this.registry}
+    this.chestInventory.on('changed', () => this.updateBlockdata());
+    this.chestIW = new InventoryWindow({inventory:this.chestInventory, registry:this.registry});
 
-    # allow shift-click to transfer items between these two inventories
-    this.chestIW.linkedInventory = this.playerInventory
-    #this.playerIW.linkedInventory = this.chestInventory # TODO: need to reach into voxel-inventory-dialog?
+    // allow shift-click to transfer items between these two inventories
+    this.chestIW.linkedInventory = this.playerInventory;
+    //this.playerIW.linkedInventory = this.chestInventory # TODO: need to reach into voxel-inventory-dialog?
 
-    chestCont = this.chestIW.createContainer()
-
-
+    chestCont = this.chestIW.createContainer();
   }
 
-  loadBlockdata: (x, y, z) -> {
-    if not this.blockdata?
-      console.log 'voxel-blockdata not loaded, voxel-chest persistence disabled'
-      return
+  loadBlockdata(x, y, z) {
+    if (!this.blockdata) {
+      console.log('voxel-blockdata not loaded, voxel-chest persistence disabled');
+      return;
+    }
 
-    bd = this.blockdata.get x, y, z
-    console.log 'activeBlockdata=',JSON.stringify(bd)
-    if bd?
-      console.log 'load existing at ',x,y,z
-      # TODO: better way to 'load' into an inventory than setting all slots?
-      newInventory = Inventory.fromString(bd.inventory)
-      console.log 'newInventory='+JSON.stringify(newInventory)
-      for itemPile, i in newInventory.array  # TODO: if smaller than current?
-        console.log 'load chest',i,itemPile
-        this.chestInventory.set i, itemPile
-    else
-      console.log 'new empty inventory at ',x,y,z
-      bd = {inventory: this.chestInventory.toString()}
-      this.blockdata.set x, y, z, bd
+    let bd = this.blockdata.get(x, y, z);
+    console.log('activeBlockdata=',JSON.stringify(bd));
+    if (bd) {
+      console.log('load existing at ',x,y,z);
+      // TODO: better way to 'load' into an inventory than setting all slots?
+      const newInventory = Inventory.fromString(bd.inventory)
+      console.log('newInventory='+JSON.stringify(newInventory));
+      for (let itemPile of newInventory.array) { // TODO: if smaller than current?
+        console.log('load chest',i,itemPile);
+        this.chestInventory.set(i, itemPile);
+      }
+    } else {
+      console.log('new empty inventory at ',x,y,z);
+      bd = {inventory: this.chestInventory.toString()};
+      this.blockdata.set(x, y, z, bd);
+    }
 
-    this.activeBlockdata = bd
-    console.log 'activeBlockdata 2=',JSON.stringify(this.activeBlockdata)
+    this.activeBlockdata = bd;
+    console.log('activeBlockdata 2=',JSON.stringify(this.activeBlockdata));
+  }
 
-  open: (target) -> {
-    this.chestInventory.clear()
+  open(target) {
+    this.chestInventory.clear();
+    this.loadBlockdata(target.voxel.x, target.voxel.y, target.voxel.z);
 
-    [x, y, z] = target.voxel
-    this.loadBlockdata(x, y, z)
+    InventoryDialog.open(target);
+  }
 
-    super()
+  updateBlockdata() {
+    console.log('update with activeBlockdata=',JSON.stringify(this.activeBlockdata));
+    if (!this.activeBlockdata) return;
 
-  updateBlockdata: () -> {
-    console.log 'update with activeBlockdata=',JSON.stringify(this.activeBlockdata)
-    return if not this.activeBlockdata?
+    console.log('chestInventory=',this.chestInventory.toString());
+    this.activeBlockdata.inventory = this.chestInventory.toString();
+  }
 
-    console.log 'chestInventory=',this.chestInventory.toString()
-    this.activeBlockdata.inventory = this.chestInventory.toString()
-
-  close: () -> {
-    delete this.activeBlockdata
-    super()
+  close() {
+    delete this.activeBlockdata;
+    InventoryDialog.close();
+  }
+}
